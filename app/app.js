1bda14c93d6139280c5533924fa77240b1577150
Seven.Chen <humen1@gmail.com>
2015-10-07 11:34:58 +0800

    add  app.js  index.js

--- app/app.js
+++ app/app.js
@@ -1,3 +1,9 @@
 /**
  * Created by seven on 2015/10/6.
  */
+
+function App(game){
+ if(!(this instanceof App)) return new App(game);
+}
+
+exports = module.exports = App;
--- app/index.js
+++ app/index.js
@@ -2,3 +2,28 @@
  * Created by seven on 2015/10/6.
  */
 
+var Phaser = require('phaser');
+var App = require('./app');
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'Game-Container', {
+    preload: preload,
+    create: create,
+    update: update,
+    render: render
+});
+
+function preload(){
+
+}
+
+function create(){
+
+}
+
+function update(){
+
+}
+
+function render(){
+
+}
